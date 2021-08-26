1513e218e6defa0d2ccad1e4cdfd13bba33e6068
Andrey_Del_Bello <andrey.delbello2002@gmail.com>
2021-08-26 20:03:42 +0000

    Semana 5 Exercicios

---
+++ ExerFor2.java
@@ -0,0 +1,12 @@
+public class ExerFor2{
+    //Imprima a soma de 1 até 1000.
+
+    public void soma(){
+        int z = 0;
+        for(int n=1;n<=1000;n++){
+            z=z+n;
+            //z=z+n
+        }
+        System.out.println("Soma de 1 até 1000: "+ z);
+    }
+}
